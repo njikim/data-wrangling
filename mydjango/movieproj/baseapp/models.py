b44d03f04f1ed53357d3c3c6ddd00a2b0584025c
njikim <eunjikim214@gmail.com>
2019-05-03 18:19:17 +0900

    jupyter & some django update

--- mydjango/movieproj/baseapp/models.py
+++ mydjango/movieproj/baseapp/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class Movie(models.Model):
-    code = models.IntegerField()
-    title = models.CharField(max_length=100)
-    date = models.DateField()
-    genre = models.CharField(max_length=100)
+
+# class Movie(models.Model):
+#     year = models.IntegerField()
+#     rank = models.IntegerField()
+#     title = models.CharField(max_length=250)
+#     date = models.CharField(max_length=250)
+#     sales = models.CharField(max_length=250)
+#     sales_share = models.CharField(max_length=250)
+#     cum_sales = models.CharField(max_length=250)
+#     num_audience = models.CharField(max_length=250)
+#     cum_audience = models.CharField(max_length=250)
+#     num_screen = models.CharField(max_length=250)
+#     showing_freq = models.CharField(max_length=250)
+#     repre_nation = models.CharField(max_length=250)
+#     nation = models.CharField(max_length=250)
+#     producer = models.CharField(max_length=250)
+#     distributor = models.CharField(max_length=250)
+#     grade = models.CharField(max_length=250)
+#     genre = models.CharField(max_length=250)
+#     director = models.CharField(max_length=250)
+#     actor = models.CharField(max_length=250)
+    
