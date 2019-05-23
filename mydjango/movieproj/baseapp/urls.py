78d1e84051a393a0305390d6e93a6f50e9d3f55a
njikim <eunjikim214@gmail.com>
2019-05-23 09:44:08 +0900

    deleted

--- mydjango/movieproj/baseapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from baseapp import views
- 
-urlpatterns = [
-    path('', views.home),
-    path('genre', views.genre),
-    path('genre/result', views.genre_result),
-
-]
